729df147ebed5dd03447c3a97c6a023cfadf898b
Yusri Yusron <yusriyusron0530@gmail.com>
2020-01-30 09:51:17 +0700

    Update dropdown using JQuery

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname+"/views/index.html")
 })
 
-app.get('/provinces', (request, response) => {
+app.post('/provinces', (request, response) => {
     config.connect((error) => {
         if (error) throw error
 
@@ -27,17 +27,10 @@ app.get('/provinces', (request, response) => {
 })
 
 app.post('/regencies', (request, response) => {
+    //Get request from client
     provinceId = request.body.province_id
     console.log('Kamu memilih nomor '+provinceId)
-
-    // let selectProvinces = 'SELECT * FROM regencies WHERE province_id = '+provinceId
-    // config.query(selectProvinces, (error, result) => {
-    //     if (error) throw error
-    //     response.json(result)
-    // })
-})
-
-app.get('/regencies', (request, response) => {
+    
     let selectProvinces = 'SELECT * FROM regencies WHERE province_id = '+provinceId
     config.query(selectProvinces, (error, result) => {
         if (error) throw error
